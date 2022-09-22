767074211dedb8b874f1a418131ed845dd64771a
Heverton Lourenço <heverton30stm@gmail.com>
2022-09-22 00:44:05 -0300

    baguncinha no código

--- GanhoDeCapital.py
+++ GanhoDeCapital.py
@@ -3,20 +3,21 @@ from Deque import *
 
 def desfaz_operacao(fila):
     estado_anterior = deque.delete_last()
-    if estado_anterior == fila:
-        estado_anterior = deque.delete_last()
-        fila = FilaArray(dados=estado_anterior)
+    if estado_anterior[0] == fila:
+        estado_anterior[0] = deque.delete_last()
+        fila = FilaArray(dados=estado_anterior[0])
         deque.add_last(estado_anterior)
     else:
-        fila = FilaArray(dados=estado_anterior)
+        fila = FilaArray(dados=estado_anterior[0])
     return fila
 
-def add_deque(fila):
-    if deque.size() < 10:
-        deque.add_last(fila)
+def add_deque(fila, saldo, lucro):
+    estado = [fila, saldo, lucro]
+    if deque.size() <= 10:
+        deque.add_last(estado)
     else:
         deque.delete_first()
-        deque.add_last(fila)
+        deque.add_last(estado)
 
 operacao = None
 fila = FilaArray()
@@ -25,7 +26,7 @@ deque = DequeArray()
 
 while operacao != 'fim':
     transacao = input(
-        "Digite 'compra' ou 'venda', qntd de ações, valor das ações OU FIM para finalizar as transações: ").split()
+        "Digite 'compra' ou 'venda', qntd de ações, valor das ações. 'FIM' ou '<' para finalizar as transações: ").split()
     if transacao[0].lower() != 'fim' and transacao[0].lower() != '<':
         operacao = transacao[0].lower()
         acao = int(transacao[1])
@@ -36,20 +37,19 @@ while operacao != 'fim':
             for i in range(acao):
                 fila.enqueue(valor)
                 saldo -= valor
-            add_deque(fila.fila_to_list())
+            add_deque(fila.fila_to_list(), saldo, lucro)
 
         elif operacao == 'venda' and acao <= fila.size():
             for i in range(acao):
                 lucro += valor - fila.dequeue()
                 saldo += valor
-            add_deque(fila.fila_to_list())
-
+            add_deque(fila.fila_to_list(), saldo, lucro)
+        
+        elif acao > fila.size():
+            print(f"\nErro! você não possui ações suficientes para fazer esta operação!")
+            print(f"Disponível: {fila.size()}")
         else:
-            if acao > fila.size():
-                print(f"\nErro! você não possui ações suficientes para fazer esta operação!")
-                print(f"Disponível: {fila.size()}")
-            else:
-                print("Operação inválida! Válidas: 'compra', 'venda', 'fim' ou '<'(desfazer operação anterior).")
+            print("Operação inválida! Válidas: 'compra', 'venda', 'fim' ou '<'(desfazer operação anterior).")
 
     elif transacao[0] == '<':
         fila = desfaz_operacao(fila.fila_to_list())        
